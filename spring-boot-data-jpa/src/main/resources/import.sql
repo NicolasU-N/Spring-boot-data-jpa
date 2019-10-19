f4e3c189c9da446a21403c98d8f2eb564537762c
Nicolás Useche <nico311021@gmail.com>
2019-10-19 10:51:00 -0500

    Commit Inicial

---
+++ spring-boot-data-jpa/src/main/resources/import.sql
@@ -0,0 +1,5 @@
+INSERT INTO clientes (id , nombre, apellido, email, create-at )
+VALUES(1,'Jorge','Gomez','jorge@gmail.com','2015-08-14')
+
+INSERT INTO clientes (id , nombre, apellido, email, create-at )
+VALUES(2,'Luis','Rojas','Luis@gmail.com','2015-08-14')
